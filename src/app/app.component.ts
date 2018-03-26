75b31cf2704532597d2868855761bf5057ab1ed3
Robinson Severo <robinsonvs@gmail.com>
2018-03-25 23:07:34 -0300

    commit 25032018 2300

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { trigger,animate,style,transition,keyframes } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+  styleUrls: ['./app.component.css'],
+  animations:[
+    trigger("moveInLeft",[
+       transition("void=> *",[style({transform:"translateX(300px)"}),
+         animate(200,keyframes([
+          style({transform:"translateX(300px)"}),
+          style({transform:"translateX(0)"})
+          ]))]),
+    transition("*=>void",[style({transform:"translateX(0px)"}),
+         animate(100,keyframes([
+          style({transform:"translateX(0px)"}),
+          style({transform:"translateX(300px)"})
+      ]))])    
+    ])
+  ]
+})      
+     
 export class AppComponent {
   todoArray = [];
 
@@ -23,6 +39,12 @@ export class AppComponent {
   }
 
   todoSubmit(value:any) {
-      console.log(value)
+    if (value !== ""){
+      this.todoArray.push(value.todo)
+      //this.todo
+    } else {
+      alert('Field required ""')
+    }
+    //console.log(value)
   }
 }
